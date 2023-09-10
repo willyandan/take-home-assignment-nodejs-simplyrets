3b868659ba3c23c811bf1dddb3edaf45cae642a3
willyandan <willyan.dantunes@gmail.com>
2023-09-10 19:21:52 -0300

    add class validator to Property

--- src/entities/Property.ts
+++ src/entities/Property.ts
@@ -1,22 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsInt, IsOptional, IsPositive, IsString } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Property {
   @PrimaryGeneratedColumn({ type: 'int' })
   id: number;
 
+  @IsString()
   @Column({ type: 'text' })
   address: string;
 
+  @IsPositive()
   @Column({ type: 'decimal' })
   price: number;
 
+  @IsInt()
+  @IsPositive()
   @Column({ type: 'smallint' })
   bedrooms: number;
 
+  @IsInt()
+  @IsPositive()
   @Column({ type: 'smallint' })
   bathrooms: number;
 
+  @IsOptional()
   @Column({ type: 'text', nullable: true })
   type: string | null;
 }
